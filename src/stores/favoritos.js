4a7dc80e536814a6191e223e07242b95e469f56b
Cristian Belalcazar <CristianBelalcazar9511@gmail.com>
2023-09-29 07:32:18 -0500

    bebidas-v1

---
+++ src/stores/favoritos.js
@@ -0,0 +1,73 @@
+import { ref, watch, onMounted, computed } from 'vue'
+import { defineStore } from 'pinia'
+import  {useBebidasStore} from './bebidas'
+import { useModalStore } from './modal'
+import { useNotificacionStore } from './notificaciones'
+
+
+export const useFavoritosStore = defineStore('favoritos', () => {
+
+       const bebidas = useBebidasStore()
+       const modal = useModalStore()
+       const favoritos = ref([])
+       const notificaciones = useNotificacionStore()
+
+       onMounted(() => {
+         favoritos.value = JSON.parse(localStorage.getItem('favoritos')) ?? []
+       })
+
+       watch(favoritos, () => {
+          sincronizarLocalStorage()
+
+       }, {
+           deep:true
+       })
+
+       function sincronizarLocalStorage()  {
+         localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
+       }
+
+       function existeFavorito() {
+          const favoritosLocalStorage = JSON.parse(localStorage.getItem('favoritos')) ?? []
+          return favoritosLocalStorage.some(favorito => favorito.idDrink === bebidas.receta.idDrink)
+       }
+
+       function eliminarFavorito() {
+         favoritos.value = favoritos.value.filter(favorito => favorito.idDrink !== bebidas.receta.idDrink)
+
+         notificaciones.mostrar = true 
+        notificaciones.texto = 'Eliminado de Favoritos'
+
+        
+       }
+
+       function agregarFavorito() {
+        favoritos.value.push(bebidas.receta)
+
+        notificaciones.mostrar = true 
+        notificaciones.texto = 'Se agrego a favoritos'
+
+       
+       }
+
+       function handleClickFavorito()  {
+          
+         if(existeFavorito(bebidas.receta.idDrink)) {
+            eliminarFavorito()
+         } else {
+            agregarFavorito()
+            
+         }
+         modal.modal = false
+
+       }
+
+       const noFavorito = computed(() => favoritos.value.length === 0)
+
+       return {
+          favoritos,
+          handleClickFavorito,
+          existeFavorito,
+          noFavorito
+       }
+})
